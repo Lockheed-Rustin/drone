d9b2337fc276a6b949c3da7d35970fe29e485b2c
Zendri Matteo <83336385+ZendriXXX@users.noreply.github.com>
2024-12-03 11:57:02 +0100

    add helper description

--- src/helper.rs
+++ src/helper.rs
@@ -4,6 +4,15 @@ use wg_2024::{
     packet::{Packet, PacketType},
 };
 
+/*
+Get the fragment:id of the given packet
+
+Parameters:
+packet:Packet -> package from which to take the fragment id
+
+Returns:
+u64 -> the fragment_id of the packet
+*/
 pub fn get_fragment_id(packet: &Packet) -> u64 {
     match &packet.pack_type {
         PacketType::MsgFragment(f) => f.fragment_index,
@@ -11,6 +20,15 @@ pub fn get_fragment_id(packet: &Packet) -> u64 {
     }
 }
 
+/*
+Get the NodeId of the current hop
+
+Parameters:
+packet:Packet -> package from which to take the current hop node
+
+Returns:
+Option<NodeId> -> returns the NodeId of the current hop if exists, None otherwise
+*/
 pub fn get_hop(packet: &Packet) -> Option<NodeId> {
     packet
         .routing_header
@@ -19,6 +37,15 @@ pub fn get_hop(packet: &Packet) -> Option<NodeId> {
         .cloned()
 }
 
+/*
+Get the NodeId of the next hop
+
+Parameters:
+packet:Packet -> package from which to take the current hop node
+
+Returns:
+Option<NodeId> -> returns the NodeId of the current hop if exists, None otherwise
+*/
 pub fn get_next_hop(packet: &Packet) -> Option<NodeId> {
     packet
         .routing_header
@@ -27,6 +54,16 @@ pub fn get_next_hop(packet: &Packet) -> Option<NodeId> {
         .cloned()
 }
 
+/*
+Determines if the packet that is a MsgFragment should be dropped based on the pdr
+
+Parameters:
+packet:Packet -> the packet that could be dropped
+pdr: f32 -> the packet drop probability
+
+Returns:
+bool -> return if the packet should be dropped
+*/
 pub fn should_drop(packet: &Packet, pdr: f32) -> bool {
     match &packet.pack_type {
         PacketType::MsgFragment(_) => thread_rng().gen_range(0.0..1.0) <= pdr,
