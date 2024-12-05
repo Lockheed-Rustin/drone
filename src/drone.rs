f2f97e2890f1e82132d2de59bed0a16b5c850f00
DiCesareDaniele <dicesaredaniele7@gmail.com>
2024-12-05 09:06:41 +0100

    refactor forward_packet forward_fragment and fixed send_nack

--- src/drone.rs
+++ src/drone.rs
@@ -1,30 +1,31 @@
 use crate::helper;
 use crossbeam_channel::{select, Receiver, Sender};
+use rand::{thread_rng, Rng};
 use std::collections::HashMap;
 use wg_2024::controller::{DroneCommand, DroneEvent};
-use wg_2024::drone::{Drone};
+use wg_2024::drone::Drone;
 use wg_2024::network::{NodeId, SourceRoutingHeader};
-use wg_2024::packet::{FloodRequest, FloodResponse, Nack, NackType, NodeType, Packet, PacketType};
-
-pub type FloodArchive = HashMap<NodeId, u64>;
+use wg_2024::packet::{FloodResponse, Nack, NackType, NodeType, Packet, PacketType};
 
 pub struct LockheedRustin {
-    pub id: NodeId,
-    pub controller_send: Sender<DroneEvent>,
-    pub controller_recv: Receiver<DroneCommand>,
-    pub packet_recv: Receiver<Packet>,
-    pub packet_send: HashMap<NodeId, Sender<Packet>>,
-    pub pdr: f32,
-    pub flood_archive: FloodArchive,
+    id: NodeId,
+    controller_send: Sender<DroneEvent>,
+    controller_recv: Receiver<DroneCommand>,
+    packet_recv: Receiver<Packet>,
+    packet_send: HashMap<NodeId, Sender<Packet>>,
+    pdr: f32,
+    flood_archive: HashMap<NodeId, u64>,
 }
 
 impl Drone for LockheedRustin {
-    fn new(id: NodeId,
-           controller_send: Sender<DroneEvent>,
-           controller_recv: Receiver<DroneCommand>,
-           packet_recv: Receiver<Packet>,
-           packet_send: HashMap<NodeId, Sender<Packet>>,
-           pdr: f32,) -> Self {
+    fn new(
+        id: NodeId,
+        controller_send: Sender<DroneEvent>,
+        controller_recv: Receiver<DroneCommand>,
+        packet_recv: Receiver<Packet>,
+        packet_send: HashMap<NodeId, Sender<Packet>>,
+        pdr: f32,
+    ) -> Self {
         Self {
             id,
             controller_recv,
@@ -46,6 +47,7 @@ impl Drone for LockheedRustin {
                 recv(self.controller_recv) -> command => {
                     if let Ok(command) = command {
                         if let DroneCommand::Crash = command {
+                            self.handle_crash();
                             return;
                         }
                         self.handle_command(command);
@@ -57,175 +59,146 @@ impl Drone for LockheedRustin {
 }
 
 impl LockheedRustin {
-
-    ///Handle the given command appropriately
-    ///
-    /// Parameters:
-    /// command:DroneCommand -> command to handle
+    /// Handle the given command.
     fn handle_command(&mut self, command: DroneCommand) {
         match command {
+            DroneCommand::RemoveSender(node_id) => {
+                self.packet_send.remove(&node_id);
+            }
             DroneCommand::AddSender(node_id, sender) => {
                 self.packet_send.insert(node_id, sender);
             }
             DroneCommand::SetPacketDropRate(pdr) => self.pdr = pdr,
             DroneCommand::Crash => unreachable!(),
-            DroneCommand::RemoveSender(node_id) => {
-                self.packet_send.remove(&node_id);
+        }
+    }
+
+    /// Handle the given packet.
+    /// Check whether the routing is correct and, if it's correct, forward the packet appropriately.
+    /// Otherwise, it sends a Nack packet to the packet sender instead.
+    fn handle_packet(&mut self, packet: Packet) {
+        match packet.pack_type {
+            PacketType::MsgFragment(_) => match self.check_routing(&packet.routing_header) {
+                Ok(_) => self.forward_fragment(packet),
+                Err(nack_type) => self.handle_drop(packet, nack_type),
+            },
+            PacketType::FloodRequest(_) => self.handle_flood_request(packet),
+            _ => match self.check_routing(&packet.routing_header) {
+                Ok(_) => self.forward_packet(packet),
+                Err(_) => self
+                    .controller_send
+                    .send(DroneEvent::ControllerShortcut(packet))
+                    .unwrap(),
             },
         }
     }
 
-
-    /// Handle the given packet
-    /// Check whether the routing is correct and, if it's correct, forward the packet appropriately
-    /// Otherwise, it sends a Nack packet to the packet sender instead
-    /// Parameters:
-    /// packet:Packet -> packet to handle
-    fn handle_packet(&mut self, packet: Packet) {
-        match packet.pack_type {
-            PacketType::FloodRequest(ref flood_request) => self.handle_flood_request(packet.clone(), flood_request.clone()),
-            _ => {
-                if let Err(nack) = self.check_routing(&packet) {
-                    return match packet.pack_type {
-                        PacketType::Nack(_) => {}
-                        _ => self.send_nack(packet, nack),
-                    };
-                }
-                if let PacketType::MsgFragment(_) = packet.pack_type {
-                    self.forward_packet(packet);
-                }
-                else {
-                    self.forward_ctrl_packet(packet)
-                }
-            }
-        }
+    /// Handle the drone crashing.
+    fn handle_crash(&mut self) {
+        todo!()
     }
 
     /// Check if there has been an error in routing.
-    /// Check if the packet has been delivered to the right destination (itself) and if the next hop is valid
-    ///
-    /// Parameters:
-    /// packet:Packet -> packet to forward
-    ///
-    /// Returns:
-    /// Result<NodeId, Nack> -> the NodeId if the packet as well delivered and the next hop is valid
-    ///                     -> an appropriate Nack for the relative problem that would have occurred during the routing otherwise
-    fn check_routing(&self, packet: &Packet) -> Result<NodeId, Nack> {
-        match helper::get_hop(&packet) {
-            Some(hop_index) if hop_index == self.id => match helper::get_next_hop(&packet) {
+    /// Check if the packet has been delivered to the right destination (itself) and if the next hop is valid.
+    fn check_routing(&self, header: &SourceRoutingHeader) -> Result<NodeId, NackType> {
+        match helper::get_hop(&header) {
+            Some(hop_index) if hop_index == self.id => match helper::get_next_hop(&header) {
                 Some(next_hop) => {
                     if !self.packet_send.contains_key(&next_hop) {
-                        Err(Nack {
-                            fragment_index: helper::get_fragment_id(packet),
-                            nack_type: NackType::ErrorInRouting(next_hop),
-                        })
+                        Err(NackType::ErrorInRouting(next_hop))
                     } else {
                         Ok(next_hop)
                     }
                 }
-                None => Err(Nack {
-                    fragment_index: helper::get_fragment_id(packet),
-                    nack_type: NackType::DestinationIsDrone,
-                }),
+                None => Err(NackType::DestinationIsDrone),
             },
-            _ => Err(Nack {
-                fragment_index: helper::get_fragment_id(packet),
-                nack_type: NackType::UnexpectedRecipient(self.id),
-            }),
+            _ => Err(NackType::UnexpectedRecipient(self.id)),
         }
     }
 
-    /// Forward the given packet via the network
-    /// Parameters:
-    /// packet:Packet -> packet to forward
+    /// Forward the fragment.
+    fn forward_fragment(&self, packet: Packet) {
+        if thread_rng().gen_range(0.0..=1.0) <= self.pdr {
+            self.handle_drop(packet, NackType::Dropped);
+        } else {
+            self.forward_packet(packet);
+        }
+    }
+
+    /// Forward all packets.
     fn forward_packet(&self, mut packet: Packet) {
-        if helper::should_drop(&packet, self.pdr) {
-            let fragment_index = helper::get_fragment_id(&packet);
-            return self.send_nack(
-                packet,
-                Nack {
-                    fragment_index,
-                    nack_type: NackType::Dropped,
-                },
-            );
-        }
-        let next_hop = helper::get_next_hop(&packet).unwrap();
+        let next_hop = helper::get_next_hop(&packet.routing_header).unwrap();
         packet.routing_header.hop_index += 1;
+        // todo: add check if next_hop is in neighbor
         match self.packet_send[&next_hop].send(packet.clone()) {
             Ok(_) => {
-                self.controller_send.send(DroneEvent::PacketSent(packet)).unwrap();
-            }
-            Err(mut packet) => {
-                let fragment_index = helper::get_fragment_id(&packet.0);
-                packet.0.routing_header.hop_index -= 1;
-                self.send_nack(
-                    packet.0,
-                    Nack {
-                        fragment_index,
-                        nack_type: NackType::ErrorInRouting(next_hop),
-                    },
-                );
-            }
-        }
-    }
-
-    /// Create a new nack packet using the given Nack and the received packet to get the information about the session_id and the routing
-    ///
-    /// Parameters:
-    /// packet:Packet -> the packet containing the session_id and the routing information
-    /// nack:Nack -> the nack to forward
-    fn send_nack(&self, packet: Packet, nack: Nack) {
-        //create Nack packet
-        let mut path: Vec<NodeId> = packet.routing_header.hops.iter().enumerate()
-            .filter(|(i, _)| {*i <= packet.routing_header.hop_index})
-            .map(|(_, elem)| *elem)
-            .collect();
-        path.reverse();
-
-        let nack_packet = Packet {
-            pack_type: PacketType::Nack(nack),
-            routing_header: SourceRoutingHeader {
-                hop_index: 1,
-                hops: path,
-            },
-            session_id: packet.session_id,
-        };
-
-        //forward Nack Packet
-        self.forward_ctrl_packet(nack_packet)
-    }
-
-    /// Forward all special packets that should not be dropped and that need to be sent to the simulation controller to avoid a crashed drone in their path.
-    ///
-    /// Parameters:
-    /// packet:Packet -> the packet to forward
-    fn forward_ctrl_packet(&self, mut packet: Packet) {
-        let next_hop = helper::get_next_hop(&packet).unwrap();
-        packet.routing_header.hop_index += 1;
-        match self.packet_send[&next_hop].send(packet.clone()) {
-            Ok(_) => {
-                self.controller_send.send(DroneEvent::PacketSent(packet)).unwrap();
+                self.controller_send
+                    .send(DroneEvent::PacketSent(packet))
+                    .unwrap();
             }
             Err(_) => {
-                self.controller_send.send(DroneEvent::ControllerShortcut(packet)).unwrap();
+                // this means that the drone has crashed
+                packet.routing_header.hop_index -= 1;
+                self.handle_drop(packet, NackType::ErrorInRouting(next_hop));
             }
         }
     }
 
+    /// Create a new nack packet using the given Nack and the received packet to get the information about the session_id and the routing.
+    /// If the packet cannot be dropped it's sent to the controller.
+    fn handle_drop(&self, packet: Packet, nack_type: NackType) {
+        match packet.pack_type {
+            PacketType::MsgFragment(ref fragment) => {
+                // create Nack path
+                if packet.routing_header.hops.is_empty() {
+                    return;
+                }
+                let mut hops = packet
+                    .routing_header
+                    .hops
+                    .iter()
+                    .cloned()
+                    .take(packet.routing_header.hop_index + 1)
+                    .rev()
+                    .collect::<Vec<_>>();
+                // force first id to be this drone id to fix unexpected recepient errors
+                hops[0] = self.id;
 
-    /// Handle the given flood_request
-    ///
-    /// Parameters:
-    /// packet:Packet -> the packet containing the session_id and the routing information
-    /// flood_request:FloodRequest -> the flood request to forward
-    fn handle_flood_request(&mut self,  mut packet: Packet, mut flood_request: FloodRequest) {
+                self.forward_packet(Packet {
+                    pack_type: PacketType::Nack(Nack {
+                        fragment_index: fragment.fragment_index,
+                        nack_type,
+                    }),
+                    routing_header: SourceRoutingHeader { hop_index: 0, hops },
+                    session_id: packet.session_id,
+                });
+
+                if let NackType::Dropped = nack_type {
+                    self.controller_send
+                        .send(DroneEvent::PacketDropped(packet))
+                        .unwrap();
+                }
+            }
+            _ => self
+                .controller_send
+                .send(DroneEvent::ControllerShortcut(packet))
+                .unwrap(),
+        }
+    }
+
+    /// Handle the given flood_request.
+    fn handle_flood_request(&mut self, mut packet: Packet) {
+        let PacketType::FloodRequest(mut flood_request) = packet.pack_type else {
+            return;
+        };
         let sender_id = flood_request.path_trace.last().unwrap().0;
         flood_request.path_trace.push((self.id, NodeType::Drone));
         packet.pack_type = PacketType::FloodRequest(flood_request.clone());
 
         match self.flood_archive.get(&flood_request.initiator_id) {
             Some(&flood_id) if flood_id == flood_request.flood_id => {
-                self.flood_archive.insert(flood_request.initiator_id.clone(), flood_request.flood_id);
+                self.flood_archive
+                    .insert(flood_request.initiator_id.clone(), flood_request.flood_id);
 
                 let flood_response_packet = Packet {
                     pack_type: PacketType::FloodResponse(FloodResponse {
@@ -234,27 +207,31 @@ impl LockheedRustin {
                     }),
                     routing_header: SourceRoutingHeader {
                         hop_index: 1,
-                        hops: flood_request.path_trace.iter()
-                            .map(|(node_id, _)| {*node_id})
+                        hops: flood_request
+                            .path_trace
+                            .iter()
+                            .map(|(node_id, _)| *node_id)
                             .collect(),
                     },
                     session_id: packet.session_id,
                 };
 
-                self.forward_ctrl_packet(flood_response_packet)
-            },
+                self.forward_packet(flood_response_packet)
+            }
             _ => {
-                self.flood_archive.insert(flood_request.initiator_id.clone(), flood_request.flood_id);
+                self.flood_archive
+                    .insert(flood_request.initiator_id.clone(), flood_request.flood_id);
 
                 for (node_id, sender) in self.packet_send.clone() {
                     if node_id != sender_id {
-                        if let Ok(_) = sender.send(packet.clone()){
-                            self.controller_send.send(DroneEvent::PacketSent(packet.clone())).unwrap();
+                        if let Ok(_) = sender.send(packet.clone()) {
+                            self.controller_send
+                                .send(DroneEvent::PacketSent(packet.clone()))
+                                .unwrap();
                         }
                     }
                 }
             }
         }
     }
-
 }
--- src/helper.rs
+++ src/helper.rs
@@ -1,64 +1,11 @@
-use rand::{thread_rng, Rng};
-use wg_2024::{
-    network::NodeId,
-    packet::{Packet, PacketType},
-};
+use wg_2024::network::{NodeId, SourceRoutingHeader};
 
-
-///Get the fragment:id of the given packet
-///
-/// Parameters:
-/// packet:Packet -> package from which to take the fragment id
-///
-/// Returns:
-/// u64 -> the fragment_id of the packet
-pub fn get_fragment_id(packet: &Packet) -> u64 {
-    match &packet.pack_type {
-        PacketType::MsgFragment(f) => f.fragment_index,
-        _ => 0,
-    }
-}
-
-///Get the NodeId of the current hop
-///
-/// Parameters:
-/// packet:Packet -> package from which to take the current hop node
-///
-/// Returns:
-/// Option<NodeId> -> returns the NodeId of the current hop if exists, None otherwise
-pub fn get_hop(packet: &Packet) -> Option<NodeId> {
-    packet
-        .routing_header
-        .hops
-        .get(packet.routing_header.hop_index)
-        .cloned()
+/// Get the NodeId of the current hop
+pub fn get_hop(header: &SourceRoutingHeader) -> Option<NodeId> {
+    header.hops.get(header.hop_index).cloned()
 }
 
 /// Get the NodeId of the next hop
-///
-/// Parameters:
-/// packet:Packet -> package from which to take the current hop node
-///
-/// Returns:
-/// Option<NodeId> -> returns the NodeId of the current hop if exists, None otherwise
-pub fn get_next_hop(packet: &Packet) -> Option<NodeId> {
-    packet
-        .routing_header
-        .hops
-        .get(packet.routing_header.hop_index + 1)
-        .cloned()
-}
-
-/// Determines if the packet that is a MsgFragment should be dropped based on the pdr
-/// Parameters:
-/// packet:Packet -> the packet that could be dropped
-/// pdr: f32 -> the packet drop probability
-///
-/// Returns: 
-/// bool -> return if the packet should be dropped
-pub fn should_drop(packet: &Packet, pdr: f32) -> bool {
-    match &packet.pack_type {
-        PacketType::MsgFragment(_) => thread_rng().gen_range(0.0..1.0) <= pdr,
-        _ => false,
-    }
+pub fn get_next_hop(header: &SourceRoutingHeader) -> Option<NodeId> {
+    header.hops.get(header.hop_index + 1).cloned()
 }
