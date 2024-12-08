70f917452059dd17d9b1abe94829d31cf5ab1190
DiCesareDaniele <dicesaredaniele7@gmail.com>
2024-12-08 20:54:58 +0100

    minor fixes before the fair

--- src/helper.rs
+++ src/helper.rs
@@ -9,3 +9,10 @@ pub fn get_hop(header: &SourceRoutingHeader) -> Option<NodeId> {
 pub fn get_next_hop(header: &SourceRoutingHeader) -> Option<NodeId> {
     header.hops.get(header.hop_index + 1).cloned()
 }
+
+/// Get the simplified version of the route of a FloodResponse
+pub fn simplify_hops(hops: &mut Vec<u8>) {
+    let src_id = hops[0];
+    let cycle_index = hops.iter().rposition(|hop| *hop == src_id).unwrap();
+    hops.drain(0..cycle_index);
+}
